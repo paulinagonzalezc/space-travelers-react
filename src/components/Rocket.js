53f57226b538e90d7aa27bd353bb27c6db6ad4ff
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-12-20 14:02:09 -0600

    Add action and UI for booking and canceling rocket

--- src/components/Rocket.js
+++ src/components/Rocket.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { bookRocket } from '../redux/rockets/rockets';
 
 const Rocket = (props) => {
-  const { name, description, image } = props;
+  const {
+    id, name, description, image, reserved,
+  } = props;
+  const dispatch = useDispatch();
+  const reserveRocket = () => {
+    dispatch(bookRocket(id));
+  };
   return (
     <div className="rocket-container">
       <img className="rocket-image" alt="rocket" src={image} />
       <div className="rocket-details">
         <h2>{name}</h2>
-        <p>{description}</p>
-        <button type="button" className="rocket-reserve-btn">Reserve Rocket</button>
+        <p className="rocket-details">
+          {reserved && (
+          <span>
+            Reserved
+          </span>
+          )}
+          {' '}
+          {description}
+        </p>
+        {!reserved ? (
+          <button type="button" className="rocket-reserve-btn" onClick={reserveRocket}>Reserve Rocket</button>
+        ) : (
+          <button type="button" className="rocket-cancel-btn" onClick={reserveRocket}>Cancel Reserve</button>
+        )}
       </div>
     </div>
   );
@@ -19,6 +39,8 @@ Rocket.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default Rocket;
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,12 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const GET = 'space-travelers-react/rocketsReducer/GET';
+const BOOK = 'space-travelers-react/rocketsReducer/BOOK';
+
+// Book rocket
+const reserveRocket = (state, payload) => {
+  const newState = state.map((rocket) => {
+    if (rocket.id !== payload) return rocket;
+    return { ...rocket, reserved: !rocket.reserved };
+  });
+  return newState;
+};
 
 // Reducer
 const rocketsReducer = (state = [], action) => {
   switch (action.type) {
     case `${GET}/fulfilled`:
       return action.payload;
+    case BOOK:
+      return reserveRocket(state, action.payload);
     default:
       return state;
   }
@@ -15,6 +27,11 @@ const rocketsReducer = (state = [], action) => {
 // Action creator
 export const getRockets = (payload) => ({
   type: GET,
+  payload,
+});
+
+export const bookRocket = (payload) => ({
+  type: BOOK,
   payload,
 });
 
