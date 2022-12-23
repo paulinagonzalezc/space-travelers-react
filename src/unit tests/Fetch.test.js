1cc1de2e7eeb02bffbd12fd315f2fbfb0ebc7b20
David Zambrano <david._101@hotmail.com>
2022-12-22 19:44:19 -0500

    Add test for reducers

--- src/unit tests/Fetch.test.js
+++ src/unit tests/Fetch.test.js
@@ -1,22 +1,24 @@
 import { Provider } from 'react-redux';
-import renderer from 'react-test-renderer';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import MissionContainer from '../components/MissionContainer';
 import store from '../redux/configureStore';
-import '@testing-library/jest-dom'
-import { render } from '@testing-library/react';
 
 global.fetch = jest.fn(() => Promise.resolve({
- json: () => Promise.resolve([{ mission_id: 48, mission_name: 'Elom', description:'this is a test'},
- { mission_id: 55, mission_name: 'Tori', description:'this is a test also'}]),
+  json: () => Promise.resolve([{ mission_id: 48, mission_name: 'Elom', description: 'this is a test' },
+    { mission_id: 55, mission_name: 'Tori', description: 'this is a test also' }]),
 }));
 
 afterEach(() => {
- fetch.mockClear();
+  fetch.mockClear();
 });
 
 test('renders content', () => {
- const component = render(<Provider store={store}>
-   <MissionContainer />
-   </Provider>)
-expect(fetch).toHaveBeenCalledTimes(1);
-})
+  const component = render(
+    <Provider store={store}>
+      <MissionContainer />
+    </Provider>,
+  );
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(component).toBeDefined();
+});
