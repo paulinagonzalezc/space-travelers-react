982fdc477b75bffd5b2627f460613f9c68f46ee9
Paulina Gonzalez <paulinagzzc@gmail.com>
2022-12-23 00:30:39 -0600

    Fix linter

--- src/__tests__/Rocket.test.js
+++ src/__tests__/Rocket.test.js
@@ -33,18 +33,20 @@ describe('Rocket component test', () => {
   afterEach(cleanup);
 
   it('increments counter', async () => {
-    const { getByTestId } = render(<Provider store={store}>
-      <BrowserRouter>
-        <Rocket
-          id={1}
-          name="Falcon1"
-          description="The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth."
-          image="https://imgur.com/DaCfMsj.jpg"
-          reserved={false}
-        />
-      </BrowserRouter>
-      ,
-                                   </Provider>);
+    const { getByTestId } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Rocket
+            id={1}
+            name="Falcon1"
+            description="The Falcon 1 was an expendable launch system privately developed and manufactured by SpaceX during 2006-2009. On 28 September 2008, Falcon 1 became the first privately-developed liquid-fuel launch vehicle to go into orbit around the Earth."
+            image="https://imgur.com/DaCfMsj.jpg"
+            reserved={false}
+          />
+        </BrowserRouter>
+        ,
+      </Provider>,
+    );
 
     fireEvent.click(getByTestId('button-up'));
     expect(getByTestId('button-up')).toHaveTextContent('Reserve Rocket');
